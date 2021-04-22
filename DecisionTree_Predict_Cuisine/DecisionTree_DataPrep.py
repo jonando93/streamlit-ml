35995692e898343beabfbcb43c5129fa51a3a694
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2021-04-22 15:31:45 +0700

    add multiple new variables/objects

--- DecisionTree_Predict_Cuisine/DecisionTree_DataPrep.py
+++ DecisionTree_Predict_Cuisine/DecisionTree_DataPrep.py
@@ -1,7 +1,60 @@
 # Import Libraries
 import streamlit as st
+import pandas as pd
+from DecisionTree_DataUnderstanding import df_train
+from DecisionTree_DataUnderstanding import df_test
+
+# Create variable for all entries (number of rows)
+row_entries = len(df_train['id'])
+
+# Create set list for all ingredients
+all_ingredients = set()
+for ingredients in df_train['ingredients']:
+    all_ingredients = all_ingredients | set(ingredients)
+len(all_ingredients)
+
+# Create 2 variables for total_ingredients and average_ingredients
+total_ingredients = 0
+for ingredients in df_train['ingredients']:
+    total_ingredients += len(ingredients)
+average_ingredients = total_ingredients / row_entries
+
 
 # Section 3 - Data Preparation
 def app():
     st.title("Section 3 - Data Preparation")
-    st.write("This is the first section (change this later)")
+    st.write("""
+    ## **Data Checking & Cleaning** (If Necessary)
+    """)
+    st.subheader("Type of Cuisine Distribution")
+    st.write(df_train['cuisine'].value_counts())
+    st.write("""
+    There are a total of {} unique type of cuisine in this dataset, and there are total of 6 type of
+    cuisine that have significant difference than the other type of cuisine. Which are Italian, 
+    Mexican, Southern US, Indian, Chinese and French (all of them are above 2000).
+    """.format(len(df_train['cuisine'].unique())))
+
+    st.write("""---""")
+
+    st.subheader("Unique Ingredients")
+    st.write(len(all_ingredients))
+    st.write("""
+    There are a total of {} unique ingredients in this dataset.
+    """.format(len(all_ingredients)))
+
+    st.write("""---""")
+
+    st.subheader("Missing Values")
+    st.write("**df_train**")
+    st.write(df_train.isnull().sum())
+    st.write("**df_test**")
+    st.write(df_test.isnull().sum())
+
+    st.write("""---""")
+
+    st.write("""
+    The datasets are cleaned and ready to be processed in the next phase which is Exploratory 
+    Data Analysis
+    """)
+
+
