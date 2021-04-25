711a7665c9ceb58d4501455a736075d95f2078ea
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2021-04-25 15:51:40 +0700

    add one-hot encoding

--- DecisionTree_Predict_Cuisine/DecisionTree_MLModel.py
+++ DecisionTree_Predict_Cuisine/DecisionTree_MLModel.py
@@ -1,7 +1,39 @@
 # Import Libraries
 import streamlit as st
+import pandas as pd
+from DecisionTree_DataPrep import df_train
+from sklearn.preprocessing import MultiLabelBinarizer
+
+df_train_copy = df_train.copy()
+
+# Create object for MultiLabelBinarizer
+mlb = MultiLabelBinarizer(sparse_output=True)
+
+# Create new DataFrame for One-Hot Encoding method
+df_train_onehot = df_train_copy.join(
+    pd.DataFrame.sparse.from_spmatrix(
+        mlb.fit_transform(df_train_copy.pop('ingredients')),
+        index=df_train_copy.index,
+        columns=mlb.classes_))
+print(df_train_onehot)
+print(df_train['ingredients'].head())
+print(df_train_copy)
 
 # Section 5 - Machine Learning Model
 def app():
     st.title("Section 5 - Machine Learning Model")
-    st.write("This is the first section (change this later)")
+    st.write("""
+    In this section, I am going to build the Decision Tree Model to predict cuisine based on 
+    ingredients. In the previous section, I have prepared the dataset that will be used in 
+    building the model. 
+    """)
+
+    st.subheader("One-Hot Encoding")
+    st.write("""
+    In our dataset, the 'ingredients' column are in list format.
+    """)
+    st.write(df_train['ingredients'].head(3))
+    st.write("""
+    I will use **one-hot encoding** method to make separate columns for each ingredients.
+    """)
+    st.write(df_train_onehot.head(3))
