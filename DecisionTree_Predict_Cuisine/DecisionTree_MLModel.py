e15b503b925075c1356767d89d166e77e72196b9
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2021-04-27 01:53:35 +0700

    add Decision Tree model

--- DecisionTree_Predict_Cuisine/DecisionTree_MLModel.py
+++ DecisionTree_Predict_Cuisine/DecisionTree_MLModel.py
@@ -4,25 +4,42 @@ import pandas as pd
 import numpy as np
 from DecisionTree_DataPrep import df_train
 from DecisionTree_DataPrep import df_train_onehot
+from DecisionTree_DataPrep import df_test_onehot
 from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
 import graphviz
 from sklearn.tree import export_graphviz
 
-# Create 2 new variables for cuisine and ingredients
-cuisines = df_train_onehot['cuisine'] # y_train
-ingredients = df_train_onehot.iloc[:, 2:] # x_train
+# ----- TEST DATASET -----
+# Since the TEST DATASET does not include cuisine column, I will use TRAIN DATASET instead.
+ingredients_test = df_test_onehot.iloc[:, 1:]
 
-# Adjust width of the Decision Tree model (and print its accuracy)
-for n in range(4,30):
-    depth = n+1
+# ----- TRAIN DATASET -----
+# I will split the Train dataset to create 4 variables (xy train & xy test)
 
-    # Create object for decision tree model called mango_tree
-    mango_tree = tree.DecisionTreeClassifier(criterion='entropy', max_depth=depth)
-    mango_tree.fit(ingredients, cuisines)
-    pred_cuisines = mango_tree.predict(ingredients)
-    print("Accuracy is ", accuracy_score(pred_cuisines, cuisines)*100, "% for Depth:", depth)
+# Create 2 new variables for cuisine and ingredients (x & y)
+cuisines = df_train_onehot['cuisine'] # y
+ingredients = df_train_onehot.iloc[:, 2:] # x
 
+x_train, x_test, y_train, y_test = train_test_split(ingredients, cuisines, test_size=0.2, random_state=69)
+
+# Create object for decision tree model called mango_tree
+mango_tree = tree.DecisionTreeClassifier(class_weight=None,
+                                         criterion='gini',
+                                         max_depth=None,
+                                         max_features=None,
+                                         max_leaf_nodes=None,
+                                         min_impurity_decrease=0.0,
+                                         min_impurity_split=None,
+                                         min_samples_leaf=1,
+                                         min_samples_split=2,
+                                         min_weight_fraction_leaf=0.0,
+                                         random_state=None,
+                                         splitter='best')
+mango_tree.fit(x_train, y_train)
+y_pred = mango_tree.predict(x_test)
+print("Accuracy is ", accuracy_score(y_pred, y_test)*100)
 
 # Section 5 - Machine Learning Model
 def app():
